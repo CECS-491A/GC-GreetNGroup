007d7853a923eaf8f8f786c032aaa565a1391fec
Eric <eric.lee0051@gmail.com>
2018-11-19 20:49:51 -0800

    Authorization Implementation Restart

---
+++ GreetNGroup/GreetNGroup/Claim_Controls/ClaimsPool.cs
@@ -0,0 +1,14 @@
+namespace GreetNGroup.Claim_Controls
+{
+    public static class ClaimsPool
+    {
+        public enum Claims
+        {
+            CanViewEvents,
+            CanCreateEvents,
+            CanFriendUsers,
+            CanBlacklistUsers,
+            AdminRights
+        };
+    }
+}
