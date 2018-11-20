f7815e78791dd77cb7c662f1f31d788617fc7b89
dylanchhin <dylanchhinn@gmail.com>
2018-11-20 11:15:25 -0800

    Basic User Class Without Interfaces and Test Project

---
+++ GreetNGroup/Test/UnitTestProject1/UnitTestProject1/User.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+/*
+    Basic user account class with all fields needed for a registered account     
+*/
+namespace GreetNGroup.User
+{
+    public class User
+    {
+        private string UserName;
+        private string PassWord;
+        private string City;
+        private string State;
+        private string Country;
+        private string DateofBirth;
+        private string SecurityQuestion;
+        private string SecurityAnswer;
+
+        public User(string userN, string pword, string city, string state, string country, string DOB, string securityQ, string securityA)
+        {
+            UserName = userN;
+            PassWord = pword;
+            City = city;
+            State = state;
+            Country = country;
+            DateofBirth = DOB;
+            SecurityQuestion = securityQ;
+            SecurityAnswer = securityA;
+        }
+        public string GetUserName()
+        {
+            return UserName;
+        }
+    }
+}
---
+++ GreetNGroup/Test/UnitTestProject1/UnitTestProject1/UserTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using GreetNGroup.User;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UserTest
+    {
+        User Dylan = new User("dylanchhin123@gmail.com", "123", "Lakewood", "CA", "USA", "12/25/1996", 
+                                "What is your favorite book?", "Cat in the Hat");
+        [TestMethod]
+        public void GetUserName_Pass()
+        {
+            // Arrange
+            string expected = "dylanchhin123@gmail.com";
+            string actual;
+
+            // Act
+            actual = Dylan.GetUserName();
+
+            // Assert
+            Assert.AreEqual(expected, actual);
+        }
+        [TestMethod]
+        public void GetUserName_Fail()
+        {
+            // Arrange
+            string expected = "bob@gmail.com";
+            string actual;
+
+            // Act
+            actual = Dylan.GetUserName();
+
+            // Assert
+            Assert.AreNotEqual(expected, actual);
+        }
+    }
+}
