cf3440a696fead629adcd785885c7e3e7ae622d6
dylanchhin <dylanchhinn@gmail.com>
2018-11-25 13:44:26 -0800

    Started on User management functions
    Started different user management class
    Edited class User class name
    started user management testing

---
+++ GreetNGroup/UnitTest/UserManage/UserManageTest.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using GreetNGroup.User;
+using GreetNGroup.UserManage;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTest.UserManage
+{
+    [TestClass]
+    public class UserManageTest
+    {
+        [TestMethod]
+        public void CreateAccount_Pass()
+        {
+            UserAccount Dylan = UserManager.addAccount("dylan","lakewood","CA","USA","12/26/1996");
+
+            UserAccount Dylan2 = new UserAccount("dylan", "", "", "", "lakewood", "CA", "USA", "12/26/1996", "", "", "");
+
+            Assert.AreEqual(Dylan, Dylan2);
+
+        }
+    }
+}
