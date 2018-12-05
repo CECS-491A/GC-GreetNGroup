ebd2c49655c84b0b977ea597eda6328e5b2f0c0e
Erleh <eric.lee0051@gmail.com>
2018-12-05 00:03:36 -0800

    Basic SELECT query sender

--- GreetNGroup/GreetNGroup/Data Access/DataBaseQueries.cs
+++ GreetNGroup/GreetNGroup/Data Access/DataBaseQueries.cs
@@ -5,7 +5,36 @@ using System.Web;
 
 namespace GreetNGroup.Data_Access
 {
-    public class DataBaseQueries
+    public static class DataBaseQueries
     {
+        // String to connect to database
+        private static string _connectionString = "greetngroupdb.cj74stlentvn.us-west-1.rds.amazonaws.com";
+
+        /// <summary>
+        /// To generate SELECT statements to send to the database and return data
+        ///
+        /// generic "SELECT x FROM x WHERE x"
+        /// </summary>
+        /// <param name="selectArg"></param> what to SELECT from
+        /// <param name="fromArg"></param> what to FROM from
+        /// <param name="whereArg"></param>
+        /// <returns></returns>
+        public string QuerySelectWhere(string selectArg, string fromArg, string whereArg)
+        {
+            string q = "SELECT " + selectArg + " FROM " + fromArg + " WHERE " + whereArg ";";
+            using (SqlConnection connection = new SqlConnection(connectionString))
+            {
+                using (SqlCommand cmd = new SqlCommand(q), connection)
+                {
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            return reader[selectArg];
+                        }
+                    }
+                }
+            }
+        }
     }
 }
