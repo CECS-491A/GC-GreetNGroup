542518a7396e7fef236e7e24d6fe39426366e6f9
Erleh <eric.lee0051@gmail.com>
2018-12-05 01:02:37 -0800

    update to query code

--- GreetNGroup/GreetNGroup/Data Access/DataBaseQueries.cs
+++ GreetNGroup/GreetNGroup/Data Access/DataBaseQueries.cs
@@ -7,8 +7,13 @@ namespace GreetNGroup.Data_Access
 {
     public static class DataBaseQueries
     {
+        //
+        // all of the following is not using entity framework --plain sql server code -- not agnostic
+        //
+        
         // String to connect to database
-        private static string _connectionString = "greetngroupdb.cj74stlentvn.us-west-1.rds.amazonaws.com";
+        private static string _connectionString = "Server=greetngroupdb.cj74stlentvn.us-west-1.rds.amazonaws.com;" +
+                                                  "Database=;User Id=gucci;Password=password123!;";
 
         /// <summary>
         /// To generate SELECT statements to send to the database and return data
