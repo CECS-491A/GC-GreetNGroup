fe7310c791de63e0c189ca8f289b6e8727624c7f
dylanchhin <dylanchhinn@gmail.com>
2018-12-05 21:04:31 -0800

    User Class

---
+++ GreetNGroup/GreetNGroup/DataBase Classes/UserContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+namespace GreetNGroup.DataBase_Classes
+{
+    public class UserContext : DbContext
+    {
+        public UserContext() : base()
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+    }
+        
+}
