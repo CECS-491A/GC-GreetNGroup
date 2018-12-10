fe5e60393591ff0fb0d2d4f6f2f08a46088330f0
Winn <winnmoo@gmail.com>
2018-12-09 17:53:38 -0800

    Cleaned the if statements in the UpdateUser function

--- GreetNGroup/GreetNGroup/Data Access/CheckQueries.cs
+++ GreetNGroup/GreetNGroup/Data Access/CheckQueries.cs
@@ -223,18 +223,9 @@ namespace GreetNGroup.Data_Access
 
         public static void UpdateUser(string UserID, List<string> attributeContents)
         {
+            List<string> currentAttributes = new List<string>();
             var ctx = new GreetNGroupContext();
-            string firstname = "";
-            string lastname = "";
-            string username = "";
-            string password = "";
-            string city = "";
-            string state = "";
-            string country = "";
-            string DOB = "";
-            string securityquestion = "";
-            string securityanswer = "";
-            string isactivated = "";
+
             //Try statement to fill the variables with user's current attributes
             try
             {
@@ -242,17 +233,18 @@ namespace GreetNGroup.Data_Access
                 {
                     var userToUpdate = ctx.UserTables
                                    .Where(s => s.UserId == UserID).Single();
-                    firstname = userToUpdate.FirstName;
-                    lastname = userToUpdate.LastName;
-                    username = userToUpdate.UserName;
-                    password = userToUpdate.Password;
-                    city = userToUpdate.City;
-                    state = userToUpdate.State;
-                    country = userToUpdate.Country;
-                    DOB = userToUpdate.DoB.ToString();
-                    securityquestion = userToUpdate.SecurityQuestion;
-                    securityanswer = userToUpdate.SecurityAnswer;
-                    isactivated = userToUpdate.isActivated.ToString();
+
+                    currentAttributes.Add(userToUpdate.FirstName);
+                    currentAttributes.Add(userToUpdate.LastName);
+                    currentAttributes.Add(userToUpdate.UserName);
+                    currentAttributes.Add(userToUpdate.Password);
+                    currentAttributes.Add(userToUpdate.City);
+                    currentAttributes.Add(userToUpdate.State);
+                    currentAttributes.Add(userToUpdate.Country);
+                    currentAttributes.Add(userToUpdate.DoB.ToString());
+                    currentAttributes.Add(userToUpdate.SecurityQuestion);
+                    currentAttributes.Add(userToUpdate.SecurityAnswer);
+                    currentAttributes.Add(userToUpdate.isActivated.ToString());
 
                 }
             }
@@ -263,49 +255,9 @@ namespace GreetNGroup.Data_Access
             //For loop to update the attributes with new values, if there are values to update it to
             for (int i = 0; i < attributeContents.Count; i++)
             {
-                if (i == 0 && attributeContents[i] != null)
+                if (attributeContents[i] != null)
                 {
-                    firstname = attributeContents[i];
-                }
-                else if (i == 1 && attributeContents[i] != null)
-                {
-                    lastname = attributeContents[i];
-                }
-                else if (i == 2 && attributeContents[i] != null)
-                {
-                    username = attributeContents[i];
-                }
-                else if (i == 3 && attributeContents[i] != null)
-                {
-                    password = attributeContents[i];
-                }
-                else if (i == 4 && attributeContents[i] != null)
-                {
-                    city = attributeContents[i];
-                }
-                else if (i == 5 && attributeContents[i] != null)
-                {
-                    state = attributeContents[i];
-                }
-                else if (i == 6 && attributeContents[i] != null)
-                {
-                    country = attributeContents[i];
-                }
-                else if (i == 7 && attributeContents[i] != null)
-                {
-                    DOB = attributeContents[i];
-                }
-                else if (i == 8 && attributeContents[i] != null)
-                {
-                    securityquestion = attributeContents[i];
-                }
-                else if (i == 9 && attributeContents[i] != null)
-                {
-                    securityanswer = attributeContents[i];
-                }
-                else if (i == 10 && attributeContents[i] != null)
-                {
-                    isactivated = attributeContents[i];
+                    currentAttributes[i] = attributeContents[i];
                 }
             }
             //Try statement update the user in the database
@@ -315,17 +267,17 @@ namespace GreetNGroup.Data_Access
                 {
                     var userToUpdate = ctx.UserTables
                                    .Where(s => s.UserId == UserID).Single();
-                    userToUpdate.FirstName = firstname;
-                    userToUpdate.LastName = lastname;
-                    userToUpdate.UserName = username;
-                    userToUpdate.Password = password;
-                    userToUpdate.City = city;
-                    userToUpdate.State = state;
-                    userToUpdate.Country = country;
-                    userToUpdate.DoB = Convert.ToDateTime(DOB);
-                    userToUpdate.SecurityQuestion = securityquestion;
-                    userToUpdate.SecurityAnswer = securityanswer;
-                    userToUpdate.isActivated = isactivated.Equals("true");
+                    userToUpdate.FirstName = currentAttributes[0];
+                    userToUpdate.LastName = currentAttributes[1];
+                    userToUpdate.UserName = currentAttributes[2];
+                    userToUpdate.Password = currentAttributes[3];
+                    userToUpdate.City = currentAttributes[4];
+                    userToUpdate.State = currentAttributes[5];
+                    userToUpdate.Country = currentAttributes[6];
+                    userToUpdate.DoB = Convert.ToDateTime(currentAttributes[7]);
+                    userToUpdate.SecurityQuestion = currentAttributes[8];
+                    userToUpdate.SecurityAnswer = currentAttributes[9];
+                    userToUpdate.isActivated = currentAttributes[11].Equals("true");
                     ctx.SaveChanges();
                 }
                 
