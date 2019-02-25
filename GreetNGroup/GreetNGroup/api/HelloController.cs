44fdee97759bfaf4c143768b89f634c4127bec68
Winn <winnmoo@gmail.com>
2019-02-24 22:52:39 -0800

    Added helloworld

---
+++ GreetNGroup/GreetNGroup/api/HelloController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace GreetNGroup.api
+{
+    [Route("api/hello")]
+    public class HelloController : ApiController
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
