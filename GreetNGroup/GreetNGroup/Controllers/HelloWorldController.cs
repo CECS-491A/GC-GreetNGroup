9b8c5469e83113e6e35513f52ce89e27c3e7d7de
Ipsaeju <36467114+Ipsaeju@users.noreply.github.com>
2019-02-25 15:20:37 -0800

    Created GNGLogger.cs

---
+++ GreetNGroup/GreetNGroup/Controllers/HelloWorldController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace GreetNGroup.Controllers
+{
+    public class HelloWorldController : ApiController
+    {
+        [HttpGet]
+        public string HelloWorld()
+        {
+            return "Hello World";
+        }
+
+    }
+}
---
+++ GreetNGroup/GreetNGroup/Logging/GNGLogger.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Diagnostics.Tracing;
+
+namespace GreetNGroup.Logging
+{
+    public class GNGLogger
+    {
+        
+    }
+}
