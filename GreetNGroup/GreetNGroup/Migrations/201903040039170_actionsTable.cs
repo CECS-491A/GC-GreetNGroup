8671985a9fbff1cf9ec0851c143106ec06f1a65c
Erleh <eric.lee0051@gmail.com>
2019-03-03 16:47:23 -0800

    Completes base dataaccess tables and migrations

---
+++ GreetNGroup/GreetNGroup/Migrations/201903040039170_actionsTable.cs
@@ -0,0 +1,26 @@
+namespace GreetNGroup.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class actionsTable : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Action",
+                c => new
+                    {
+                        ActionId = c.String(nullable: false, maxLength: 128),
+                        ActionName = c.String(),
+                    })
+                .PrimaryKey(t => t.ActionId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Action");
+        }
+    }
+}
