22fd59f7699c64dabe96d6c88453b106f2358053
Ipsaeju <36467114+Ipsaeju@users.noreply.github.com>
2019-03-04 02:32:45 -0800

    Added test class for logger

--- GreetNGroup/GreetNGroup/Logging/GNGLogger.cs
+++ GreetNGroup/GreetNGroup/Logging/GNGLogger.cs
@@ -11,21 +11,27 @@ namespace GreetNGroup.Logging
 {
     public class GNGLogger
     {
-        private const string LOGS_FOLDERPATH = "../GreetNGroup/Logs/";
+        private const string LOGS_FOLDERPATH = "C:\\Users\\Yuki\\Documents\\GitHub\\GreetNGroup\\GreetNGroup\\GreetNGroup\\Logs\\";
+        private static LogIDGenerator logIDGenerator = new LogIDGenerator();
+        private Dictionary<string, int> listOfIDs = logIDGenerator.GetLogIDs();
+        private string currentLogPath;
         public GNGLogger()
         {
-
+            CreateNewLog();
         }
 
         public void CreateNewLog()
         {
             bool logExists = CheckForExistingLog();
-            if(logExists == false)
+            string currentDate = DateTime.Now.ToString("dd-MM-yyyy");
+            if (logExists == false)
             {
-                string currentDate = DateTime.Now.ToString("dd.MM.yyyy");
+                
                 try
                 {
-                    File.Create(LOGS_FOLDERPATH + currentDate + "_gnglog.json");
+                    var newLog = File.Create(LOGS_FOLDERPATH + currentDate + "_gnglog.json");
+                    currentLogPath = LOGS_FOLDERPATH + currentDate + "_gnglog.json";
+                    newLog.Close();
                 }
                 catch (IOException e)
                 {
@@ -33,50 +39,210 @@ namespace GreetNGroup.Logging
                 }
                 
             }
+            else
+            {
+                currentLogPath = LOGS_FOLDERPATH + currentDate + "_gnglog.json";
+            }
         }
 
         public bool CheckForExistingLog()
         {
-            string currentDate = DateTime.Now.ToString("dd.MM.yyyy");
+            string currentDate = DateTime.Now.ToString("dd-MM-yyyy");
             bool logExists = File.Exists(LOGS_FOLDERPATH + currentDate + "_gnglog.json");
 
             return logExists;
         }
 
         [HttpPost]
-        public void LogClicksMade(string startPoint, string endpoint)
+        public bool LogClicksMade(string startPoint, string endPoint, string usersID)
         {
-            
+            bool logMade = false;
+            listOfIDs.TryGetValue("ClickEvent", out int clickLogID);
+            string clickLogIDString = clickLogID.ToString();
+            GNGLog log = new GNGLog
+            {
+                logID = clickLogIDString,
+                userID = usersID,
+                dateTime = DateTime.Now.ToString(),
+                description = startPoint + " to " + endPoint
+            };
+
+            string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+            try
+            {
+                using (StreamWriter file = File.AppendText(currentLogPath))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(file, log);
+                    logMade = true;
+                    file.Close();
+                }
+            }catch(FileNotFoundException e)
+            {
+                logMade = false;
+            }
+            return logMade;
+
         }
 
         [HttpPost]
-        public void LogErrorsEncountered()
+        public bool LogErrorsEncountered(string usersID, string errorCode, string urlOfErr)
         {
+            bool logMade = false;
+            listOfIDs.TryGetValue("ErrorEncountered", out int clickLogID);
+            string clickLogIDString = clickLogID.ToString();
+            GNGLog log = new GNGLog
+            {
+                logID = clickLogIDString,
+                userID = usersID,
+                dateTime = DateTime.Now.ToString(),
+                description = errorCode + " encountered at " + urlOfErr
+            };
 
+            string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+            try
+            {
+                using (StreamWriter file = File.AppendText(currentLogPath))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(file, log);
+                    logMade = true;
+                    file.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                logMade = false;
+            }
+            return logMade;
         }
 
         [HttpPost]
-        public void LogGNGEventsCreated()
+        public bool LogGNGEventsCreated(string usersID, string eventID)
         {
+            bool logMade = false;
+            listOfIDs.TryGetValue("EventCreated", out int clickLogID);
+            string clickLogIDString = clickLogID.ToString();
+            GNGLog log = new GNGLog
+            {
+                logID = clickLogIDString,
+                userID = usersID,
+                dateTime = DateTime.Now.ToString(),
+                description = "Event " + eventID + " created"
+            };
 
+            string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+            try
+            {
+                using (StreamWriter file = File.AppendText(currentLogPath))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(file, log);
+                    logMade = true;
+                    file.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                logMade = false;
+            }
+            return logMade;
         }
 
         [HttpPost]
-        public void LogEntryToWebsite()
+        public bool LogEntryToWebsite(string usersID, string urlEntered)
         {
+            bool logMade = false;
+            listOfIDs.TryGetValue("EntryToWebsite", out int clickLogID);
+            string clickLogIDString = clickLogID.ToString();
+            GNGLog log = new GNGLog
+            {
+                logID = clickLogIDString,
+                userID = usersID,
+                dateTime = DateTime.Now.ToString(),
+                description = usersID + " entered at " + urlEntered
+            };
 
+            string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+            try
+            {
+                using (StreamWriter file = File.AppendText(currentLogPath))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(file, log);
+                    logMade = true;
+                    file.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                logMade = false;
+            }
+            return logMade;
         }
 
         [HttpPost]
-        public void LogExitFromWebsite()
+        public bool LogExitFromWebsite(string usersID, string urlOfExit)
         {
+            bool logMade = false;
+            listOfIDs.TryGetValue("ExitFromWebsite", out int clickLogID);
+            string clickLogIDString = clickLogID.ToString();
+            GNGLog log = new GNGLog
+            {
+                logID = clickLogIDString,
+                userID = usersID,
+                dateTime = DateTime.Now.ToString(),
+                description = usersID + " exited website from " + urlOfExit
+            };
 
+            string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+            try
+            {
+                using (StreamWriter file = File.AppendText(currentLogPath))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(file, log);
+                    logMade = true;
+                    file.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                logMade = false;
+            }
+            return logMade;
         }
 
         [HttpPost]
-        public void LogAccountDeletion()
+        public bool LogAccountDeletion(string usersID)
         {
+            bool logMade = false;
+            listOfIDs.TryGetValue("ErrorEncountered", out int clickLogID);
+            string clickLogIDString = clickLogID.ToString();
+            GNGLog log = new GNGLog
+            {
+                logID = clickLogIDString,
+                userID = usersID,
+                dateTime = DateTime.Now.ToString(),
+                description = usersID + " deleted account"
+            };
 
+            string json = JsonConvert.SerializeObject(log, Formatting.Indented);
+            try
+            {
+                using (StreamWriter file = File.AppendText(currentLogPath))
+                {
+                    JsonSerializer jsonSerializer = new JsonSerializer();
+                    jsonSerializer.Serialize(file, log);
+                    logMade = true;
+                    file.Close();
+                }
+            }
+            catch (FileNotFoundException e)
+            {
+                logMade = false;
+            }
+            return logMade;
         }
 
     }
---
+++ GreetNGroup/GreetNGroup/Logging/LogIDGenerator.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GreetNGroup.Logging
+{
+    public class LogIDGenerator
+    {
+
+        /// <summary>
+        /// Method IDGenerator creates a HashSet of int values with randomly generated
+        /// values to create an ID for each event to log.
+        /// </summary>
+        /// <returns>HashSet<int> listOfIDs which holds the randomly generated int IDs</returns>
+        public Dictionary<string, int> GetLogIDs()
+        {
+            Dictionary<string, int> logIDMap = new Dictionary<string, int>();
+            logIDMap.Add("ClickEvent", 1001);
+            logIDMap.Add("ErrorEncountered", 1002);
+            logIDMap.Add("EventCreated", 1003);
+            logIDMap.Add("EntryToWebsite", 1004);
+            logIDMap.Add("ExitFromWebsite", 1005);
+            logIDMap.Add("AccountDeletion", 1006);
+
+            return logIDMap;
+        }
+    }
+}
