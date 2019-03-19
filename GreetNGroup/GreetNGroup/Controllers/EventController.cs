3400116871f789e912b11199f7fb26d1eb848506
Erleh <eric.lee0051@gmail.com>
2019-03-19 00:00:27 -0700

    Starts EventController to handle Event retrieval

---
+++ GreetNGroup/GreetNGroup/Controllers/EventController.cs
@@ -0,0 +1,28 @@
+﻿using System.Net.Http;
+using System.Web.Http;
+using GreetNGroup.DataAccess.Queries;
+
+namespace GreetNGroup.Controllers
+{
+    public class EventController : ApiController
+    {
+        /// <summary>
+        /// Returns value that has been requested for retrieval in Ok response
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public IHttpActionResult GetEventById(int id)
+        {
+            try
+            {
+                var e = DbRetrieve.GetEventById(id);
+                return Ok(e);
+            }
+            catch (HttpRequestException e)
+            {
+                // Add logging
+                return BadRequest();
+            }
+        }
+    }
+}
