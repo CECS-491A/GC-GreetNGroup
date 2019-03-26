113d35fc70eec6b5ad992a006075cda06f49cd6d
Eric <eric.lee0051@gmail.com>
2019-03-25 17:00:37 -0700

    Creates Frontend Layer

---
+++ Frontend/Vue/src/components/HelloWorld.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+    <h2>{{ info }}</h2>
+    <input type="text" v-model="search" placeholder="search for events" />
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      msg: 'GreetNGroup',
+      info: null,
+      search: ''
+    }
+  },
+  mounted () {
+    axios.get('https://api.greetngroup.com/api/hello/').then(response => (this.info = response.data))
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
