b51d11109a063460e8bb68b27e7307d541c9e811
Winn <winnmoo@gmail.com>
2019-04-23 15:24:41 -0700

    Added health route

--- Backend/ManagerLayer/LoginManagement/LoginManager.cs
+++ Backend/ManagerLayer/LoginManagement/LoginManager.cs
@@ -21,35 +21,35 @@ namespace ManagerLayer.LoginManagement
 
         public string Login(SSOUserRequest request)
         {
-            //TODO: Make the concatenation more extensible
-            //foreach property in request
+            // TODO: Make the concatenation more extensible
+            // foreach property in request
 
-            string message = "ssoUserId=" + request.ssoUserId + ";" +
+            var message = "ssoUserId=" + request.ssoUserId + ";" +
                              "email=" + request.email + ";" +
                              "timestamp=" + request.timestamp + ";";
             var hashedMessage = _cryptoService.HashHMAC(message);
-            //Check if signature is valid
+            // Check if signature is valid
             if (hashedMessage == request.signature)
             {
-                //Check if user exists
+                // Check if user exists
                 if (_userService.IsUsernameFound(request.email))
                 {
                     return "https://greetngroup.com/home/" + _jwtService.CreateToken(request.email, _userService.GetUserUid(request.email));
                 }
                 else
                 {
-                    User createdUser = new User(  //If user doesn't exist, create a placeholder user that is not activated
-                        _userService.GetNextUserID(), //UserID
-                        null, //First name
-                        null, //Last name
-                        request.email, //Username
-                        null, //City
-                        null, //State
-                        null, //Country
-                        DateTime.Now, //Minimum datetime for DOB
-                        false //IsActivated
+                    User createdUser = new User(  // If user doesn't exist, create a placeholder user that is not activated
+                        _userService.GetNextUserID(), // UserID
+                        null, // First name
+                        null, // Last name
+                        request.email, // Username
+                        null, // City
+                        null, // State
+                        null, // Country
+                        DateTime.Now, // Minimum datetime for DOB
+                        false // IsActivated
                         );
-                    _userService.InsertUser(createdUser); //Check for user acivation on home page
+                    _userService.InsertUser(createdUser); // Check for user acivation on home page
                     return _jwtService.CreateToken(request.email, createdUser.UserId);
                 }
             }
---
+++ Backend/WebApi/HealthHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace WebApi
+{
+    public class HealthHandler : DelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(
+        HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.OK)
+            {
+                Content = new StringContent("Good Health")
+            };
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(httpResponse);   // Also sets the task state to "RanToCompletion"
+            return tsc.Task;
+
+            /*
+            var httpResponse = new HttpResponseMessage(HttpStatusCode.InternalServerError)
+            {
+                Content = new StringContent("Under Maintenance")
+            };
+            var tsc = new TaskCompletionSource<HttpResponseMessage>();
+            tsc.SetResult(httpResponse);   // Also sets the task state to "RanToCompletion"
+            return tsc.Task;
+            */
+        }
+    }
+}
