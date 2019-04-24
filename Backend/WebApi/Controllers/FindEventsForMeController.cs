c3f1ee8ca573a6a5d3fce0866cd59f5b4e333080
Eric <eric.lee0051@gmail.com>
2019-04-24 13:22:31 -0700

    Adds filter classes used for findeventsforme

--- Backend/WebApi/Controllers/FindEventsForMeController.cs
+++ Backend/WebApi/Controllers/FindEventsForMeController.cs
@@ -1,17 +1,50 @@
 ﻿using System.Web.Http;
+using Gucci.ServiceLayer.Requests;
 
-namespace WebApi.Controllers
+namespace Gucci.WebApi.Controllers
 {
+
     public class FindEventsForMeController : ApiController
     {
-        /*
-        [Route("api/FindEventsForMe/{useTags}/{useDates}/{useStates}/")]
-        public IHttpActionResult GetEvents([FromBody] EventCreationRequest request, string useTags, string useDates, string useStates)
+        [HttpPost]
+        [Route("api/FindEventsForMe/")]
+        public IHttpActionResult GetEvents([FromBody] FindEventsForMeRequest request)
         {
             // make call to function with 4 params     findEvents(useTags, useDates, useStates, filter)
             // use bools to determine strategy
             // strategy will grab from filter as needed
+            if (!request.UseTags && !request.UseDates && !request.UseLocation)
+            {
+
+            }
+            else if (request.UseTags && !request.UseDates && !request.UseLocation)
+            {
+
+            }
+            else if (!request.UseTags && request.UseDates && !request.UseLocation)
+            {
+
+            }
+            else if (!request.UseTags && !request.UseDates && request.UseLocation)
+            {
+
+            }
+            else if (request.UseTags && request.UseDates && !request.UseLocation)
+            {
+
+            }
+            else if (request.UseTags && !request.UseDates && request.UseLocation)
+            {
+
+            }
+            else if (!request.UseTags && request.UseDates && request.UseLocation)
+            {
+
+            }
+            else if (request.UseTags && request.UseDates && request.UseLocation)
+            {
+
+            }
         }
-        */
     }
 }
