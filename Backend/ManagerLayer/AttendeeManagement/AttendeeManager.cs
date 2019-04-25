47b495a369e91352d7309a83f63721c45a77323d
dylanchhin <dylanchhinn@gmail.com>
2019-04-24 22:35:09 -0700

    Added join and leave event function
    need to test on actual server
    Need to add more stuff to event page

--- Backend/ManagerLayer/AttendeeManagement/AttendeeManager.cs
+++ Backend/ManagerLayer/AttendeeManagement/AttendeeManager.cs
@@ -1,16 +1,21 @@
-﻿using Gucci.DataAccessLayer.Models;
-using Gucci.ServiceLayer.Interface;
+﻿using Gucci.DataAccessLayer.Context;
 using Gucci.ServiceLayer.Services;
-using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Gucci.ManagerLayer.AttendeeManagement
 {
-    class AttendeeManager
+    public class AttendeeManager
     {
-        private AttendeesService _attendeeService = new AttendeesService();
+        private AttendeesService _attendeeService;
+        private JWTService _jWTService;
+        private readonly List<string> joinEventClaims = new List<string> ();
+        GreetNGroupContext ctx = new GreetNGroupContext();
         public AttendeeManager()
         {
+            _attendeeService = new AttendeesService();
+            _jWTService = new JWTService();
+            joinEventClaims.Add(ctx.Claims.FirstOrDefault(c => c.ClaimId.Equals(9)).ClaimName);
         }
         /// <summary>
         /// Function that inserts a attendee into the attendance table
@@ -18,24 +23,32 @@ namespace Gucci.ManagerLayer.AttendeeManagement
         /// <param name="eventId">Event id attendee is joining</param>
         /// <param name="userId">User id of the attendee</param>
         /// <returns>A string based on what happended</returns>
-        public string JoinEvent(int eventId, int userId)
+        public string JoinEvent(int eventId, string jwt)
         {
             var eventJoinedMessage = "Joined Event";
-
-            var isEventandUserValid = _attendeeService.DoesEventandUserExist(eventId, userId);
-            if(isEventandUserValid == false)
+            var userID = _jWTService.GetUserIDFromToken(jwt);
+            var hasClaims = _jWTService.CheckUserClaims(jwt, joinEventClaims);
+            if(hasClaims)
             {
-                return eventJoinedMessage = "Event or User does not exist";
+                var isEventandUserValid = _attendeeService.DoesEventandUserExist(eventId, userID);
+                if (isEventandUserValid == false)
+                {
+                    return eventJoinedMessage = "Event or User does not exist";
+                }
+                var doesAttendeeExist = _attendeeService.DoesAttendeeExist(eventId, userID);
+                if (doesAttendeeExist)
+                {
+                    return eventJoinedMessage = "You have already joined the event";
+                }
+                var attendeeJoined = _attendeeService.InsertAttendee(eventId, userID);
+                if (attendeeJoined == false)
+                {
+                    return eventJoinedMessage = "Uh Oh something went wrong";
+                }
             }
-            var doesAttendeeExist = _attendeeService.DoesAttendeeExist(eventId, userId);
-            if(doesAttendeeExist == true)
+            else
             {
-                return eventJoinedMessage = "You have already joined the event";
-            }
-            var attendeeJoined = _attendeeService.InsertAttendee(eventId, userId);
-            if(attendeeJoined == false)
-            {
-                return eventJoinedMessage = "Uh Oh something went wrong";
+                eventJoinedMessage = "Sorry you are not allowed to join events";
             }
             return eventJoinedMessage;
         }
@@ -45,21 +58,21 @@ namespace Gucci.ManagerLayer.AttendeeManagement
         /// <param name="eventId">Event id attendee is joining</param>
         /// <param name="userId">User id of the attendee</param>
         /// <returns>A string based on what happended</returns>
-        public string LeaveEvent(int eventId, int userId)
+        public string LeaveEvent(int eventId, string jwt)
         {
             var eventLeftMessage = "Left Event";
-
-            var isEventandUserValid = _attendeeService.DoesEventandUserExist(eventId, userId);
+            var userID = _jWTService.GetUserIDFromToken(jwt);
+            var isEventandUserValid = _attendeeService.DoesEventandUserExist(eventId, userID);
             if (isEventandUserValid == false)
             {
                 return eventLeftMessage = "Event or User does not exist";
             }
-            var doesAttendeeExist = _attendeeService.DoesAttendeeExist(eventId, userId);
+            var doesAttendeeExist = _attendeeService.DoesAttendeeExist(eventId, userID);
             if (doesAttendeeExist == false)
             {
                 return eventLeftMessage = "You have not joined the event";
             }
-            var attendeeLeft = _attendeeService.DeleteAttendee(eventId, userId);
+            var attendeeLeft = _attendeeService.DeleteAttendee(eventId, userID);
             if (attendeeLeft == false)
             {
                 return eventLeftMessage = "Uh Oh something went wrong";
