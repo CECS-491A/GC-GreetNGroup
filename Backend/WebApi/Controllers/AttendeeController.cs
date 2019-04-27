29912c63f96ee68deb79e73e0597730295a21d5a
dylanchhin <dylanchhinn@gmail.com>
2019-04-27 15:54:37 -0700

    Added tags and attendee list to event page

---
+++ Backend/WebApi/Controllers/AttendeeController.cs
@@ -0,0 +1,31 @@
+﻿using Gucci.ServiceLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebApi.Controllers
+{
+    public class AttendeeController : ApiController
+    {
+        private AttendeesService attendeeService = new AttendeesService();
+        [HttpGet]
+        [Route("api/attendee/{eventid}")]
+        public IHttpActionResult GetAttendeesList(int eventId)
+        {
+            try
+            {
+                //return first and last names of attendees
+                var names = attendeeService.GetAttendees(eventId);
+                return Ok(names);
+            }
+            catch (HttpRequestException e)
+            {
+                //gngLogManager.LogBadRequest(userId.ToString(), ip, url, e.ToString());
+                return BadRequest();
+            }
+        }
+    }
+}
