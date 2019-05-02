829ee8e4d28aaffa8c06bc4c7f0d86530975e052
Eric <eric.lee0051@gmail.com>
2019-05-01 17:53:38 -0700

    Finishes error catches for findeventsforme

--- Backend/WebApi/Controllers/FindEventsForMeController.cs
+++ Backend/WebApi/Controllers/FindEventsForMeController.cs
@@ -17,60 +17,65 @@ namespace Gucci.WebApi.Controllers
             var tempList = new List<Event>();
 
             var filteredEventList = new List<Event>();
-            // make call to function with 4 params     findEvents(useTags, useDates, useStates, filter)
-            // use bools to determine strategy
-            // strategy will grab from filter as needed
 
-            // null blank inputs will be caught in the frontend
-            // a secondary check is created in here for off cases
-
-            // Return list of events unfiltered
-            if (!request.UseTags && !request.UseDates && !request.UseLocation)
+            try
             {
-                filteredEventList = eventFinder.FindAllEvents();
+                // Return list of events unfiltered
+                if (!request.UseTags && !request.UseDates && !request.UseLocation)
+                {
+                    filteredEventList = eventFinder.FindAllEvents();
+                }
+                // Return list of events filtered by tags
+                else if (request.UseTags && !request.UseDates && !request.UseLocation)
+                {
+                    filteredEventList = eventFinder.FindEventByEventTags(request.Tags);
+                }
+                // Filtered by Dates
+                else if (!request.UseTags && request.UseDates && !request.UseLocation)
+                {
+                    filteredEventList = eventFinder.FindEventsByDateRange(request.StartDate, request.EndDate);
+                }
+                // Filtered by Location
+                else if (!request.UseTags && !request.UseDates && request.UseLocation)
+                {
+                    filteredEventList = eventFinder.FindEventsByState(request.State);
+                }
+                // Filtered by Tags and Dates
+                else if (request.UseTags && request.UseDates && !request.UseLocation)
+                {
+                    tempList = eventFinder.FindEventByEventTags(request.Tags);
+                    filteredEventList = eventFinder.CullEventListByDateRange(tempList,
+                        request.StartDate, request.EndDate);
+                }
+                // Filtered by Tags and Location
+                else if (request.UseTags && !request.UseDates && request.UseLocation)
+                {
+                    tempList = eventFinder.FindEventByEventTags(request.Tags);
+                    filteredEventList = eventFinder.CullEventListByState(tempList, request.State);
+                }
+                // Filtered by Dates and Location
+                else if (!request.UseTags && request.UseDates && request.UseLocation)
+                {
+                    tempList = eventFinder.FindEventsByDateRange(request.StartDate, request.EndDate);
+                    filteredEventList = eventFinder.CullEventListByState(tempList, request.State);
+                }
+                // Filtered by Tags, Dates, and Location
+                else if (request.UseTags && request.UseDates && request.UseLocation)
+                {
+                    tempList = eventFinder.FindEventByEventTags(request.Tags);
+                    tempList = eventFinder.CullEventListByState(tempList, request.State);
+                    filteredEventList =
+                        eventFinder.CullEventListByDateRange(tempList, request.StartDate, request.EndDate);
+                }
             }
-            // Return list of events filtered by tags
-            else if (request.UseTags && !request.UseDates && !request.UseLocation)
+            catch (ArgumentException argExcept) // Wrong should not be found, but if they are, catch here
             {
-                filteredEventList = eventFinder.FindEventByEventTags(request.Tags);
+                return BadRequest();
             }
-            // Filtered by Dates
-            else if (!request.UseTags && request.UseDates && !request.UseLocation)
+            catch (Exception e) // Catch all of errors bubbling up from services
             {
-                filteredEventList = eventFinder.FindEventsByDateRange(request.StartDate, request.EndDate);
+                return BadRequest();
             }
-            // Filtered by Location
-            else if (!request.UseTags && !request.UseDates && request.UseLocation)
-            {
-                filteredEventList = eventFinder.FindEventsByState(request.State);
-            }
-            // Filtered by Tags and Dates
-            else if (request.UseTags && request.UseDates && !request.UseLocation)
-            {
-                tempList = eventFinder.FindEventByEventTags(request.Tags);
-                filteredEventList = eventFinder.CullEventListByDateRange(tempList, 
-                    request.StartDate, request.EndDate);
-            }
-            // Filtered by Tags and Location
-            else if (request.UseTags && !request.UseDates && request.UseLocation)
-            {
-                tempList = eventFinder.FindEventByEventTags(request.Tags);
-                filteredEventList = eventFinder.CullEventListByState(tempList, request.State);
-            }
-            // Filtered by Dates and Location
-            else if (!request.UseTags && request.UseDates && request.UseLocation)
-            {
-                tempList = eventFinder.FindEventsByDateRange(request.StartDate, request.EndDate);
-                filteredEventList = eventFinder.CullEventListByState(tempList, request.State);
-            }
-            // Filtered by Tags, Dates, and Location
-            else if (request.UseTags && request.UseDates && request.UseLocation)
-            {
-                tempList = eventFinder.FindEventByEventTags(request.Tags);
-                tempList = eventFinder.CullEventListByState(tempList, request.State);
-                filteredEventList = eventFinder.CullEventListByDateRange(tempList, request.StartDate, request.EndDate);
-            }
-
             // Return results
             return Ok(filteredEventList);
         }
