9cea2193209cfc8da1932587fac02a644b94c3a6
Erleh <eric.lee0051@gmail.com>
2019-05-13 02:07:06 -0700

    checkinservice edits

--- Backend/ServiceLayer/Services/CheckInService.cs
+++ Backend/ServiceLayer/Services/CheckInService.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Linq;
 using Gucci.DataAccessLayer.Context;
 
 namespace Gucci.ServiceLayer.Services
@@ -10,48 +6,28 @@ namespace Gucci.ServiceLayer.Services
     public class CheckInService
     {
         // Check attendees list of current event for current user
-        /*
         public bool CheckAttendanceList(int eventId, string username)
         {
-            try
+            using (var ctx = new GreetNGroupContext())
             {
-                using (var ctx = new GreetNGroupContext())
-                {
-                    // perform search for attendees list via eventId
+                // Find if username exists within corresponding event attendee table
+                var found = ctx.Attendees.Any(events => events.EventId.Equals(eventId) && events.User.UserName.Equals(username));
 
-                    // perform comparison of attendee ids with username
-
-                    // return true if found false if not found
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
+                return found;
             }
         }
-        */
 
-        /*
+        // Check code inputted by user to id required by the event
         public bool CheckInputCode(int eventId, string input)
         {
-            try
+            using (var ctx = new GreetNGroupContext())
             {
-                using (var ctx = new GreetNGroupContext())
-                {
-                    // perform search for current event
+                // perform search for current event and compares EventCheckInCode with input
+                var verified = ctx.Events.First(events => events.EventId.Equals(eventId)).EventCheckinCode
+                   .Equals(input);
 
-                    // perform comparison of event Join code to input
-
-                    // return true if matching else false
-                }
-            }
-            catch (Exception e)
-            {
-                Console.WriteLine(e);
-                throw;
+                return verified;
             }
         }
-        */
     }
 }
